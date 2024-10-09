4ce70a9d4ae7752c0f8c39322b9d53aa9ff169a5
Muslim <130743304+muslldev@users.noreply.github.com>
2024-10-09 20:13:15 +0600

    [+]First step

--- src/main/java/com/mpbtwozeroone/piebook/HelloWorldController.java
+++ src/main/java/com/mpbtwozeroone/piebook/HelloWorldController.java
@@ -12,7 +12,7 @@ public class HelloWorldController {
         return
                 "<!DOCTYPE html>" +
                         "<html>" +
-                        "	<head><title>Hello world!</title></head>" +
+                        "	<head><title>Hello world</title></head>" +
                         "	<body><h1>Hello world!</h1></body>" +
                         "</html>"
                 ;
