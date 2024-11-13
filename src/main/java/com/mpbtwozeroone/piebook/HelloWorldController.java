65ce1de524e5f5db6c4d120a61c47b849369bd45
Muslim Kozhasov <muslim200411@gmail.com>
2024-11-13 21:05:42 +0600

    [+]First step in app

---
+++ src/main/java/com/mpbtwozeroone/piebook/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.mpbtwozeroone.piebook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("omsu")
+public class HelloWorldController {
+    @GetMapping(value = "helloworld", produces = "text/html")
+    public String helloWorld() {
+        return
+                "<!DOCTYPE html>" +
+                        "<html>" +
+                        "	<head><title>Hello world!</title></head>" +
+                        "	<body>Hello world!</body>" +
+                        "</html>"
+                ;
+    }
+}
